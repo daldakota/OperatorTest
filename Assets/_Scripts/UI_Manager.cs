ab5566320c7df7067866f1aa79076451f153369d
Ethan Thibault <wickedlystudios@gmail.com>
2018-01-13 11:44:06 -0500

    Created UI Manager, Updated all Prefabs

---
+++ Assets/_Scripts/UI_Manager.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour {
+	// This class will be for managing the UI elements
+}
