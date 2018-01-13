e78b16cb224a62cf762f53a686ae0b6689db1a4e
Ethan Thibault <wickedlystudios@gmail.com>
2018-01-13 14:30:02 -0500

    Codebook Prototype UI is in!

--- Assets/_Scripts/UI_Manager.cs
+++ Assets/_Scripts/UI_Manager.cs
@@ -11,13 +11,16 @@ public class UI_Manager : MonoBehaviour {
 	static = anyone can access it */
 	public static UI_Manager instance = null;
 
-
-	[Header("UI Object References")]
+	[Header("Monitor")]
 	// Request Monitor
 	public GameObject RequestMonitorObj;
 	private Text RequestMonitorText;
+
+	[Header("Codebook")]
 	// Codebook UI
 	public GameObject CodebookPanel;
+	private int currentCodebookPage = 0;
+	public GameObject[] CodebookPages;
 
 	void Awake(){
 		/* Singleton Shit */
@@ -41,5 +44,18 @@ public class UI_Manager : MonoBehaviour {
 	/* Codebook */
 	public void ActivateCodebook(){
 		CodebookPanel.SetActive(!CodebookPanel.activeInHierarchy);
+	}
+
+	public void CodebookPageFlip(int Direction){
+		// 0 is left, 1 is right
+		// checks to see if you are at the end of the pages
+		if (currentCodebookPage == 0 && Direction == 0){Debug.Log("No.");}
+		else if (currentCodebookPage == CodebookPages.Length -1 && Direction == 1){Debug.Log("No.");}
+		else {
+			CodebookPages[currentCodebookPage].SetActive(false);
+			if (Direction == 0){currentCodebookPage -=1;}
+			else if (Direction == 1){currentCodebookPage +=1;}
+			CodebookPages[currentCodebookPage].SetActive(true);
+		}
 	}
 }
